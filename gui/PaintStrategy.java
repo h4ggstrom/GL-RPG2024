33baed52c04a25a4560335969391ddf48359c385
haggstrom <robin2angelis@gmail.com>
2024-02-15 09:08:13 +0100

    mise à jour du chemin pour charger la room

--- gui/PaintStrategy.java
+++ gui/PaintStrategy.java
@@ -18,7 +18,7 @@ public class PaintStrategy {
     
     // Stratégie d'affichage pour la salle
     public void paint (Room room, Graphics graphics) {
-        graphics.drawImage(Utility.readImage("ressources\\room.png"), 0, 0, null);
+        graphics.drawImage(Utility.readImage("./ressources/room.png"), 0, 0, null);
     }
 
     // Stratégie d'affichage pour le joueur
