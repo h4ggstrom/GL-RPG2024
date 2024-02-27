7228d3bcc3a1f3c2d5b4c58e611fc675efafc7c2
h4ggstrom <robin2angelis@gmail.com>
2024-02-27 22:22:46 +0100

    ajout des commentaires javadoc sur 6 classes

--- config/GameConfiguration.java
+++ config/GameConfiguration.java
@@ -2,21 +2,36 @@ package config;
 
 import engine.dungeon.Position;
 
+/**
+ * Génie Logiciel - Projet RPG.
+ * 
+ * Cette classe contient les constantes nécessaires au fonctionnement du jeu.
+ * 
+ * @author thibault.terrie@etu.cyu.fr
+ * @author robin.de-angelis@etu.cyu.fr
+ * @author hayder.ur-rehman@etu.cyu.fr
+ *  
+ */
 public class GameConfiguration {
 
+    // dimensions de la fenêtre
     public static final int WINDOW_WIDTH = 1080;
 	public static final int WINDOW_HEIGHT = 720;
 	
+    // vitesse d'exécution par défaut du jeu
 	public static final int GAME_SPEED = 1;
 
-    public static final int PLAYER_DEFAULT_SPEED = 10;
+    // statistiques par défaut du joueur : vitesse de base ; dimensions du personnage ; points de vie
+    public static final int PLAYER_DEFAULT_SPEED = 10; 
     public static final int PLAYER_WIDTH = 20;
     public static final int PLAYER_HEIGHT = 40;
     public static final int PLAYER_HEALTH = 50;
     
+    // statistiques par défaut de l'arme
     public static final int WEAPON_DAMAGE = 5;
     public static final int WEAPON_RANGE = 100;
 
+    // dimensions de la salle, incluant les limites des murs
     public static final int ROOM_UPPER_LIMITATION = 52; // Le mur bloque le joueur avant cette valeur en Y
     public static final int ROOM_LOWER_LIMITATION = 624; // Le mur bloque le joueur après cette valeur en Y
     public static final int ROOM_LEFT_LIMITATION = 52; // Le mur bloque le joueur avant cette valeur en X
@@ -26,11 +41,13 @@ public class GameConfiguration {
     public static final Position GATE_UP = new Position(1026, 292); // Le pixel haut de la porte de sortie
     public static final Position GATE_DOWN = new Position(1028, 429); // Le pixel bas de la porte de sortie
 
+    // statistiques par défaut des ennemis : nombre initial par salle ; dimensions des ennemis ; points de vie
     public static final int ENEMIES_INIT_NUMBER = 4;
     public static final int ENEMY_WIDTH = 20;
     public static final int ENEMY_HEIGHT = 40;
     public static final int ENEMY_HEALTH = 30;
 
+    // correction de l'offset des coordonnées renvoyées par la classe MouseListener
     public static final int CORRECTCLICKSHIFT_X = -8; // Le MouseListener séléctionne une coordonnée en X incorrecte de 8 pixels en trop
     public static final int CORRECTCLICKSHIFT_Y = -32; // Le MouseListener séléctionne une coordonnée en Y incorrecte de 31 pixels en trop
 }
