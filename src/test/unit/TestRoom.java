ba25ad94aeefef571c12003e3fcc29c404f0fecb
h4ggstrom <robin2angelis@gmail.com>
2024-03-26 14:15:39 +0100

    ajout de tests (fontionnels) pour la Room

--- src/test/unit/TestRoom.java
+++ src/test/unit/TestRoom.java
@@ -1,5 +1,6 @@
 package test.unit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
@@ -8,7 +9,9 @@ import org.junit.Test;
 import engine.process.GameBuilder;
 import engine.dungeon.Room;
 import engine.Entity;
-import java.util.ArrayList;
+import engine.characters.Enemy;
+import engine.dungeon.Position;
+
 
 public class TestRoom {
     
@@ -35,10 +38,26 @@ public class TestRoom {
      */
     @Test
     public void testAddEntity() {
-        ArrayList<Entity> entities = room.getEntities();
-        // @TODO
-        
-        
+        int nbBefore = room.getEntities().size();
+        Position p = new Position(500, 500);
+        Entity ent = new Enemy(p);
+        room.addEntity(ent);
+        int nbAfter = room.getEntities().size();
+        assertEquals(nbBefore,nbAfter-1);
+    }
+    
+    /**
+     * Teste la suppression d'une entité à la room
+     */
+    @Test
+    public void testRmEntity() {
+        Position p = new Position(500, 500);
+        Entity ent = new Enemy(p);
+        room.addEntity(ent);
+        int nbBefore = room.getEntities().size();
+        room.removeEntity(ent);
+        int nbAfter = room.getEntities().size();
+        assertEquals(nbBefore,nbAfter+1);
     }
 
 
