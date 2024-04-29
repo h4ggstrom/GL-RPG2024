8aff5a408793845afdce31f52b90a5068bd72835
Fan2Programmation <103863802+Fan2Programmation@users.noreply.github.com>
2024-04-29 16:35:35 +0200

    ecran titre

--- src/gui/MainMenuGUI.java
+++ src/gui/MainMenuGUI.java
@@ -3,14 +3,20 @@ package gui;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 import config.GameConfiguration;
 import engine.entities.characters.Player;
+import engine.process.management.RessourceManager;
 
 public class MainMenuGUI extends JFrame {
 
+    private ImagePanel imagePanel;
     private JButton btnNewGame = new JButton("Nouvelle partie");
     private JButton btnLoadGame = new JButton("Charger une partie existante");
     private JButton btnQuitter = new JButton("Quitter");
@@ -33,6 +39,14 @@ public class MainMenuGUI extends JFrame {
             Math.max(btnNewGame.getPreferredSize().height, Math.max(btnLoadGame.getPreferredSize().height, btnQuitter.getPreferredSize().height))
         );
 
+        // Créez un ImagePanel avec votre image de fond
+        try {
+            imagePanel = new ImagePanel("./src/ressources/title.png");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         btnNewGame.setMaximumSize(buttonSize);
         btnLoadGame.setMaximumSize(buttonSize);
         btnQuitter.setMaximumSize(buttonSize);
@@ -53,13 +67,14 @@ public class MainMenuGUI extends JFrame {
         southPanel.add(buttonPanel);
         southPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
-     
-        getContentPane().add(southPanel, BorderLayout.SOUTH);
+        imagePanel.setLayout(new BorderLayout());
+        imagePanel.add(southPanel, BorderLayout.SOUTH);
 
 		setTitle("Menu Principal");
         setSize(GameConfiguration.WINDOW_WIDTH, GameConfiguration.WINDOW_HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        getContentPane().add(imagePanel);
         setLocationRelativeTo(null);
 		setVisible(true);
 	}
@@ -98,4 +113,18 @@ public class MainMenuGUI extends JFrame {
             System.exit(0);
 		}
 	}
+
+    class ImagePanel extends JPanel {
+        private BufferedImage backgroundImage;
+
+        public ImagePanel(String imagePath) throws IOException {
+            backgroundImage = RessourceManager.getInstance().getImage(imagePath);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(backgroundImage, 0, 0, this);
+        }
+    }
 }
